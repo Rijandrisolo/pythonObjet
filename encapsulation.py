bed637fea01cfe9cd2c6a24fd51715bb45d0fde0
rijarandria <rijandrisolo@gmail.com>
2023-12-13 13:31:43 +0100

    TP05 encapsulation.py

---
+++ encapsulation.py
@@ -0,0 +1,43 @@
+class Livre:
+    def __init__(self, titre, auteur, achetable=False):
+        self._titre = titre
+        self._auteur = auteur
+        self._achetable = achetable
+
+    def __str__(self):
+        if self._achetable == True:
+            return f" Titre : {self._titre} - Auteur : {self._auteur} - Achetable \n"
+        else:
+            return f" Titre : {self._titre} - Auteur : {self._auteur} \n"
+
+    @property
+    def titre(self):
+        return self.titre
+
+    @property
+    def auteur(self):
+        return self.auteur
+
+    @property
+    def achetable(self):
+        return self.achetable
+
+    @titre.setter
+    def titre(self, newtitre):
+        self._titre = newtitre
+
+    @auteur.setter
+    def auteur(self, newauteur):
+        self._auteur = newauteur
+
+    @achetable.setter
+    def achetable(self, newachetable):
+        self._achetable = newachetable
+
+
+l1 = Livre("La recette", "Francis Navarre")
+l2 = Livre("Le jardin", "Jacques Pourcel", False)
+l3 = Livre("Les Misérables", "Victor Hugo", True)
+print(l1, l2, l3)
+l1.auteur = "Laurent Pourcel"
+print(l1, l2, l3)
