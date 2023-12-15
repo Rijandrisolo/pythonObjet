16edd13a573be3ac6079d1c97a094e65eeb00a55
rijarandria <rijandrisolo@gmail.com>
2023-12-15 11:28:22 +0100

    TP13 tri avec __lt__ ok

---
+++ TP13 tri liste.py
@@ -0,0 +1,30 @@
+class Personne:
+    def __init__(self, nom, age):
+        self.nom = nom
+        self.age = age
+
+    def __str__(self):
+        return f" Personne  : {self.nom} - âge: {self.age} "
+
+    def __repr__(self):
+        return f" Personne  : {self.nom} - âge: {self.age} "
+
+    def __eq__(self, other):
+        if isinstance(other, Personne):
+            return self.nom == other.nom and self.age == other.age
+        return False
+
+    def __hash__(self):
+        return hash((self.nom, self.age))
+
+    def __lt__(self, other):
+        return self.nom < other.nom
+
+
+pers1 = Personne("Coco", 32)
+pers2 = Personne("Toto", 25)
+pers3 = Personne("Jaco", 45)
+liste = [pers1, pers2, pers3]
+tri_liste = sorted(liste)
+for liste in tri_liste:
+    print(liste)
